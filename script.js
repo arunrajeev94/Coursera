183894b723d92829a0778a781931600d9e7dbd56
Arun Rajeev <arunwork94@gmail.com>
2021-03-06 23:54:04 +0000

    update assignment

---
+++ script.js
@@ -0,0 +1,8 @@
+// STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
+// Loop over the names array and say either 'Hello' or "Good Bye"
+// using the 'speak' method or either helloSpeaker's or byeSpeaker's
+// 'speak' method.
+// using either the helloSpeaker's or byeSpeaker's 'speak' method.
+// See Lecture 50, part 1
+for (var i = 0; i < names.length; i++) {
+}
